ce62c69692c8b3d0eb8b31cb920426f4ba5ec7d1
yisuyong <yisuyong@gmail.com>
2019-05-22 13:59:26 +0900

    20190522_135926

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 kill -9 `ps -ef |grep pure-ftpd|awk '{print $2}'`
-#./src/pure-ftpd --bind ip,21 --createhomedir  --noanonymous --chrooteveryone --dontresolve --daemonize --verboselog -l mysql:/root/pureftpd-mysql.conf
-./src/pure-ftpd --bind 59.15.94.144,21 --limitrecursion 5000:500 -c 50 --createhomedir  --noanonymous --chrooteveryone --dontresolve --daemonize --verboselog -l mysql:/root/pureftpd-mysql.conf
+/usr/local/pureftpd/pure-ftpd --bind x.x.x.x,21 --limitrecursion 5000:500 -c 50 --createhomedir  --noanonymous --chrooteveryone --dontresolve --daemonize --verboselog -l mysql:/usr/local/pureftpd/pureftpd-mysql.conf
